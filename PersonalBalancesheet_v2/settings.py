e179257050f7adbb378087ef0c8dd6feb7ddadd7
萩生田 亮 <akirahagiuda@gmail.com>
2021-06-28 18:34:17 +0900

    Add whitenoise

--- PersonalBalancesheet_v2/settings.py
+++ PersonalBalancesheet_v2/settings.py
@@ -21,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1rk31xkljd58bx9l6$*temluj%sroj*8sj(i1^s$q(#0w%4pz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -52,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PersonalBalancesheet_v2.urls'
